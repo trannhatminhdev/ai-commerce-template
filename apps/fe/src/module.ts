5936eafb5c1362d6d9557be3e21da311849e92fe
trannhatminhdev <trannhatminh.dev@gmail.com>
2026-08-20 15:05:33 +0700

    chore: setup husky, lint-staged and git hooks

--- apps/fe/src/module.ts
+++ apps/fe/src/module.ts
@@ -1,19 +1,20 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { defineNuxtModule, addPlugin, createResolver } from "@nuxt/kit";
 
 // Module options TypeScript interface definition
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface ModuleOptions {}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'ai-commerce-fe',
-    configKey: 'aiCommerceFe',
+    name: "ai-commerce-fe",
+    configKey: "aiCommerceFe",
   },
   // Default configuration options of the Nuxt module
   defaults: {},
   setup(_options, _nuxt) {
-    const resolver = createResolver(import.meta.url)
+    const resolver = createResolver(import.meta.url);
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
+    addPlugin(resolver.resolve("./runtime/plugin"));
   },
-})
+});
